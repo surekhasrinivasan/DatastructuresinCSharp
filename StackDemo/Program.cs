0b935b3b153cf3ade0e65867936015c9472b8ccd
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-29 07:45:20 -0500

    Stack implementation using Array

---
+++ StackDemo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDemo
+{
+    // Stack implementation using array in C#
+    class Program
+    {
+        static void Main()
+        {
+            Stack S = new Stack(5);
+
+            S.push(10);
+            S.push(20);
+            S.push(30);
+            S.push(40);
+            S.push(50);
+
+            Console.WriteLine("Stack elements are : ");
+            S.printStack();
+
+            S.pop();
+            S.pop();
+            S.pop();
+        }
+    }
+
+    class Stack
+    {
+        private int[] elements;
+        private int top;
+        private int max;
+
+        public Stack(int size)
+        {
+            elements = new int[size];
+            top = -1;
+            max = size;
+        }
+
+        public void push(int item)
+        {
+            if (top == max - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            else
+            {
+                elements[++top] = item;
+            }
+        }
+
+        public int pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack Underflow");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("Popped element is: " + elements[top]);
+                return elements[top--];
+            }
+        }
+
+        public void printStack()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]" + elements[i]);
+                }
+            }
+        }
+    }
+}
--- StackDemo/Properties/AssemblyInfo.cs
+++ StackDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataStructuresinCSharp")]
+[assembly: AssemblyTitle("StackDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataStructuresinCSharp")]
+[assembly: AssemblyProduct("StackDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("696cfbe8-2e8e-4a2f-a706-5af1aed9b096")]
+[assembly: Guid("8e230b9a-b8ac-4d6d-b72f-4866a5abb3f6")]
 
 // Version information for an assembly consists of the following four values:
 //
