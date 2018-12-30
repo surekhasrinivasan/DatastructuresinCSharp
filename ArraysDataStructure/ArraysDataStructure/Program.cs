c9a94e72f93c2e05625aa22121f8d5020cd4ab42
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-30 06:55:31 -0500

    Arrays datastructure initial commit

---
+++ ArraysDataStructure/ArraysDataStructure/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDataStructure
+{
+    // Arrays is used to store a collection of data. 
+    // It stores a fixed-size sequential collection of elements of the same type. 
+    // A specific element in an array is accessed by an index. 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
