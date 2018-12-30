7a032acae4267262acc220dbfa44c70b446f12bb
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-30 07:27:03 -0500

    Linear Queue Datastructure - initial commit

---
+++ LinearQueueDemo/LinearQueueDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearQueueDemo
+{
+    /* Linear Queue follows FIFO (First In First Out) property, it means first inserted elements, deleted first. 
+     * In linear queue there are two pointers are used:
+        FRONT:	It points the location from where we can delete an item from linear queue.
+        REAR:	It points the location from where we can insert element into linear queue.
+    * */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
