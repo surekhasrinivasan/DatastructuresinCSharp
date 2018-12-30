963ef455414f315baa553bb358944b80edc4534f
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-30 07:17:31 -0500

    Arrays datastructure implementation

--- ArraysDataStructure/ArraysDataStructure/Program.cs
+++ ArraysDataStructure/ArraysDataStructure/Program.cs
@@ -14,6 +14,33 @@ namespace ArraysDataStructure
     {
         static void Main(string[] args)
         {
+            // Defining Array
+            int[] arr1 = new int[5];
+            arr1[0] = 10;
+            arr1[1] = 20;
+            arr1[2] = 30;
+            arr1[3] = 40;
+            arr1[4] = 50;
+
+            // inline array
+            int[] arr2 = new int[5] { 10, 20, 30, 40, 50 };
+
+            int[] arr3 = new int[] { 10, 20, 30, 40, 50, 60, 70 };
+
+            foreach (int i in arr1)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (int i in arr2)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (int i in arr3)
+            {
+                Console.WriteLine(i);
+            }            
         }
     }
 }
