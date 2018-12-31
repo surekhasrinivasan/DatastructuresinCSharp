52eb43fdd4ecc07aee8868f20132151c7b5d8603
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-31 17:09:53 -0500

    notes on Linkedlist

--- LinkedListDatastructure/LinkedListDatastructure/Program.cs
+++ LinkedListDatastructure/LinkedListDatastructure/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace LinkedListDatastructure
 {
+    /**
+     * A LinkedList is a series of objects which instead of having their references indexed (like an Array), stay together by linking to each other, in Nodes.
+     * 
+     * A LinkedList Node has basically three values: the Object's Value, a reference to the Next node, and a reference to the Previous Node.
+     * */
+
     class Program
     {
         static void Main(string[] args)
