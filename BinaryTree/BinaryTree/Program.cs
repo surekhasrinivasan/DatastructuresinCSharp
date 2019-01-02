adea2c3e6c7b129d81bceb64e0e545ea35dfc462
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-02 18:40:18 -0500

    Binary tree datastructure - initial commit

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    // A Binary tree is a tree data structure in which each node has at most two child nodes, usually distinguished as "left" and "right".
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
